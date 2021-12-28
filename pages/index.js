5e715f1be17f1aeca41352a0a7bbb1b077800c5e
yoonhero <yoonhero06@naver.com>
2021-12-28 23:23:20 +0900

    feat: #1.1 Fetching Data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
+import { useState, useEffect } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "9c4e72909e805980f8e97aa8af02d8b4";
+
 export default function Home() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const data = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+
+      setMovies(data);
+    })();
+  }, []);
   return (
     <div>
       <Seo title='Home' />
-      <h1 className='active'>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
