886a7b54216860a1f7e2c8bad6c6d16aff44abe4
yoonhero <yoonhero06@naver.com>
2021-12-28 23:40:05 +0900

    feat: #1.2 Redirect and Rewrite

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
+const API_KEY = process.env.API_KEY;
+
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path",
+        destination: "/new-blob/:path",
+        permanent: false,
+      },
+    ];
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
